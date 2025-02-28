aa2ec84ac229dd9745dcefee139878b312af7138
Nikhil Pulluri <nikhilpulluri7810@gmail.com>
2025-02-28 21:30:00 +0530

    updates

--- server/src/events/events.gateway.ts
+++ server/src/events/events.gateway.ts
@@ -1,6 +1,6 @@
 import { WebSocketGateway, SubscribeMessage, MessageBody } from '@nestjs/websockets';
 
-@WebSocketGateway(81, { cors: '*', transports: ['websocket'] })
+@WebSocketGateway(81)
 export class EventsGateway {
   @SubscribeMessage('message')
   handleEvent(@MessageBody() data: string): string {
