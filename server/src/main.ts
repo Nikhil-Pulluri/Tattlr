a38838ec842a200cd0ac2f468e51ae009f99465a
Nikhil Pulluri <nikhilpulluri7810@gmail.com>
2025-07-21 15:03:26 +0530

    cors fix

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const corsOptions: CorsOptions = {
-    origin: ["http://10.1.172.77:3000", "http://localhost:3000"], 
+    origin: ["http://10.1.172.77:3000", "http://localhost:3000", "https://tattlr.vercel.app"], 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept', 
     credentials: true
